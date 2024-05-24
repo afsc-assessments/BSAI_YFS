95f6aebbf3da3cdba215b5564e9cb059f2ba86fa
31ingrid <ingrid.spies@noaa.gov>
2024-05-24 11:09:14 -0700

    Delete src directory

--- src/make.bat
+++
@@ -1,8 +0,0 @@
-if NOT EXIST build mkdir build
-if NOT EXIST build\debug mkdir build\debug
-if NOT EXIST build\release mkdir build\release
-call admb -g fm.tpl 
-copy fm.exe build\debug
-call admb -f fm.tpl 
-copy fm.exe build\release
-
